e682df117b8328cab812b9ca196780764ef0471c
Mateus <mduraes1994@gmail.com>
2018-02-02 18:24:49 -0200

    Login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
 import { NavController } from 'ionic-angular';
 
 import { UserData } from '../../providers/user-data';
@@ -21,13 +19,9 @@ export class LoginPage {
 
   constructor(public navCtrl: NavController, public userData: UserData) { }
 
-  onLogin(form: NgForm) {
-    this.submitted = true;
-
-    if (form.valid) {
-      this.userData.login(this.login.username);
-      this.navCtrl.push(TabsPage);
-    }
+  onLogin() {
+    // this.userData.login(this.login.username);
+    this.navCtrl.push(TabsPage);
   }
 
   onSignup() {
